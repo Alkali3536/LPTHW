d52223586661640441f8f01e9b46cd1ca1681936
Alkali <alkali3536@gmail.com>
2024-10-05 23:22:11 +0100

    LPTHW 2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#A Comment, this is so you can read your program later
+#Anything After the # is ignored by python code
+
+print ("I Could Have Code Like This") #and the comment affer is ignored
+
+#You can also use a comment to "Disable" or comment out code:
+#print("This wont't work")
+
+print ("This will run. ")
